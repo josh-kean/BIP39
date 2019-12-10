19574eae80bb12aef9ffb71e435dee4ca9048926
jkean <joshkn@protonmail.com>
2019-12-10 18:14:02 -0500

    cleaned up the check_sum function

--- main.py
+++ main.py
@@ -3,35 +3,38 @@
 import hashlib as hsh
 import tkinter as tk
 import random as rand
+import sys
 
 #class containing all the hashing functions
 class HashingFunctions:
-    def __init__(self, entropy = '1', word_list = None):
-        self.word_list_length = 2048
+    def __init__(self, entropy = hsh.sha256('hello'.encode('utf-8')).hexdigest(), word_list = None):
         self.word_list = open('words.txt', 'r').readlines()
         self.word_list = [word[:-1] for word in self.word_list]
-        self.entropy = bin(int(hsh.sha256(entropy.encode('utf-8')).hexdigest(), 16))[2:]
+        self.word_list_length = len(self.word_list)
+        self.entropy = entropy #need to determine the number of bits stored in entropy
         self.result_word_list = word_list
         self.binary_seed = None
         self.passphrase = 'TREZOR'
+        self.check_sum = None
     
     def get_input(self):
         self.entropy = input('type whatever you want into the keyboard: ')
 
-    #generates entropy based on user input. it is output as the result of a sha256 hash and the entripy is given as 256 random bits
-    def create_entropy(self):
-        entropy_hash = hsh.sha256(self.entropy.encode('utf-8')).hexdigest()
-        ent = bin(int(ent, 16))
-        self.entropy = ent[2:] #removes the prefix '0b' from binary string
-        
     #checksum is created by taking the first len(ent) (in this case 256 bits)/32 bits of sha256 of entropy
-    def check_sum(self):
-        self.check_sum = self.entropy[:len(self.entropy)//32]
+    def create_check_sum(self):
+        entropy_hash = hsh.sha256(self.entropy.encode('utf-8')).hexdigest()
+        entropy_hash = entropy_hash[2:]
+        #entropy is input as a hex value
+        ent_length = sys.getsizeof(int(self.entropy, 16))
+        #takes the first to len(binary)//32 bits of the hash of the initial entropy
+        self.check_sum = entropy_hash[:ent_length//32]
+        self.check_sum = bin(int(self.check_sum, 16))[2:]
 
     def create_word_list(self):
+        self.entropy = bin(int(self.entropy, 16))[2:]
         ent_and_chk = self.entropy+self.check_sum #the initial entropy with the checksum appended to end
         list_length = len(ent_and_chk)//11
-        self.result_word_list = [self.word_list[int(self.entropy[11*x:11*(x+1)],2)] for x in range(list_length)]
+        self.result_word_list = [self.word_list[int(self.entropy[11*x:11*(x+1)],2)]+' ' for x in range(list_length)]
 
     def create_binary_seed(self):
         word_list = ''.join(self.result_word_list)
@@ -48,11 +51,11 @@ class HashingFunctions:
 
 if __name__ == '__main__':
     test1 = HashingFunctions('00000000000000000000000000000000')
-    test1.check_sum()
+    test1.create_check_sum()
     test1.create_word_list()
     test1.create_binary_seed()
-    #print(test1.result_word_list)
-    #print(test1.binary_seed)
+    print(test1.result_word_list)
+    print(test1.binary_seed)
     test2 = HashingFunctions('1', 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about')
     test2.create_binary_seed()
     print('test2', test2.binary_seed)
